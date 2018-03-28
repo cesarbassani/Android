f123703ddc6f4eb5120e1048694b9c1d03a572e3
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2018-03-27 22:19:06 -0400

    Criando ViewHolder

--- app/src/main/java/com/cesarbassani/conversormoedas/MainActivity.java
+++ app/src/main/java/com/cesarbassani/conversormoedas/MainActivity.java
@@ -2,12 +2,29 @@ package com.cesarbassani.conversormoedas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.textDollar = findViewById(R.id.text_dollar);
+        this.mViewHolder.textEduro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+    }
+
+    private static class ViewHolder {
+        EditText editValue;
+        TextView textDollar;
+        TextView textEduro;
+        Button buttonCalculate;
     }
 }
