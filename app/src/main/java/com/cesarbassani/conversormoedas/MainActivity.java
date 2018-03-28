2d0ea92de453a05640638940a94c06229f67a7b1
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2018-03-27 22:37:01 -0400

    Regras do aplicativo

--- app/src/main/java/com/cesarbassani/conversormoedas/MainActivity.java
+++ app/src/main/java/com/cesarbassani/conversormoedas/MainActivity.java
@@ -2,11 +2,12 @@ package com.cesarbassani.conversormoedas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
 
@@ -19,6 +20,26 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textDollar = findViewById(R.id.text_dollar);
         this.mViewHolder.textEduro = findViewById(R.id.text_euro);
         this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+
+        this.clearValues();
+    }
+
+    private void clearValues() {
+        this.mViewHolder.textDollar.setText("");
+        this.mViewHolder.textEduro.setText("");
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.button_calculate) {
+            Double value = Double.valueOf(this.mViewHolder.editValue.getText().toString());
+
+            this.mViewHolder.textDollar.setText(String.format("%.2f", value * 3));
+            this.mViewHolder.textEduro.setText(String.format("%.2f", value * 4));
+        }
     }
 
     private static class ViewHolder {
